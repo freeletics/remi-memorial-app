e80dcedfee794aa31a268d2c2fc2adf55c91b91c
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2018-02-06 14:37:24 +0100

    Added scaffold for adding messages with image

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,46 @@
+class MessagesController < ApplicationController
+  before_action :set_message, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @messages = Message.all
+  end
+
+  def show
+  end
+
+  def new
+    @message = Message.new
+  end
+
+  def edit
+  end
+
+  def create
+    @message = Message.new(message_params)
+
+    if @message.save
+      redirect_to @message, notice: 'Message was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @message.update(message_params)
+      redirect_to @message, notice: 'Message was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_message
+      @message = Message.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def message_params
+      params.require(:message).permit(:content, :image)
+    end
+end
